01ff58e5e68798f78bb1a66013dd01beb173ec95
RAM-BHARATH <ramtherapid007@gmail.com>
2021-01-11 12:08:16 +0530

    Completed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    string = string.toLowerCase();
+    let a = [];
+    for(i=0;i<string.length;i++){
+        if(string[i]>='a' && string[i]<='z'){
+            a.push(string[i]);
+        }
+    }
+    // console.log(a.join(''));
+    // console.log(a.reverse().join(''));
+    // console.log(string);
+    console.log(a.join('')===a.reverse().join(''));
+    if(a.join('')===a.reverse().join('')){
+        return true;
+    }
+    return false;
 }
 
 module.exports = palindromes
