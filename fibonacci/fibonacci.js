4090e0bd0c4937e3d77836ce7b53bb4b2fef1813
RAM-BHARATH <ramtherapid007@gmail.com>
2021-01-11 15:47:00 +0530

    Completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number<0){
+        return "OOPS";
+    }
+    arr=[1,1];
+    for(i=2;i<number;i++){
+        arr.push(arr[i-2]+arr[i-1]);
+    }
+    console.log(arr[number-1]);
+    return arr[number-1];
 }
 
 module.exports = fibonacci
