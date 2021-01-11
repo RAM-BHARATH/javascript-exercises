d2c24673c848f137a8be2ea397660814d2b1e92a
RAM-BHARATH <ramtherapid007@gmail.com>
2021-01-11 16:01:49 +0530

    Completed getTheTiles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objectArray) {
+    let titles = objectArray.reduce((accumulator,present,index)=>{
+        // console.log(typeof(accumulator));
+        accumulator[`${index}`]=present.title;
+        return accumulator;
+    },[]);
+    console.log(titles);
+    return titles;
 }
 
 module.exports = getTheTitles;
