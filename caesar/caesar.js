cf21898200c571eb66e3115c93780af952b2ca46
RAM-BHARATH <ramtherapid007@gmail.com>
2021-01-11 15:38:10 +0530

    Completed caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,38 @@
-const caesar = function() {
-
+const caesar = function(string,number) {
+    let strArray = string.split('');
+    number=number%26;
+    if(number<0){
+        number=26+number;
+    }
+    console.log(strArray);
+    let cipher =[];
+    strArray.forEach(char=> {
+        if((char>='A' && char<='Z')||(char>='a'&&char<='z')){
+            let asciiValue = 65;
+            if(char>='A' && char<='Z'){
+                asciiValue =(char.charCodeAt(0)+number);
+                if(asciiValue>90){
+                    asciiValue=asciiValue%90;
+                    asciiValue+=64;
+                }   
+            }
+            else if(char>='a'&&char<='z'){
+                asciiValue =(char.charCodeAt(0)+number);
+                if(asciiValue>122){
+                    asciiValue=asciiValue%122;
+                    asciiValue+=96;
+                }
+            }
+            console.log("Original ASCII Value: "+char.charCodeAt(0)+" Changed ASCII value: "+asciiValue);
+            let cipherChar = String.fromCharCode(asciiValue);
+            cipher.push(cipherChar);   
+        }
+        else{
+            cipher.push(char);
+        }
+    });
+    console.log(cipher.join(''));
+    return cipher.join('');
 }
-
+caesar('Hello, World!',75);
 module.exports = caesar
