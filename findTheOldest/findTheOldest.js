8588f37807a754e68ad250902c9c5f34e184914b
RAM-BHARATH <ramtherapid007@gmail.com>
2021-01-11 16:27:31 +0530

    Completed findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(ObjectArr) {
+    let sortArr = ObjectArr.sort((present,other)=>{
+        if(present.yearOfDeath===undefined){
+            present.yearOfDeath=new Date().getUTCFullYear();
+            console.log("Present year of death : "+present.yearOfDeath);
+        }
+        if(other.yearOfDeath===undefined){
+            other.yearOfDeath=new Date().getUTCFullYear();
+            console.log("Other year of death : "+other.yearOfDeath);
+        }
+        console.log("Present: "+present.name+" other: "+other.name);
+        return (present.yearOfDeath-present.yearOfBirth)-(other.yearOfDeath-other.yearOfBirth);
+    });
+    console.log(sortArr);
+    return sortArr[sortArr.length-1];
 }
+
 
 module.exports = findTheOldest
